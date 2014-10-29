0bc96448854419de4b16758f550fa672a9e92f68
meolu <861459320@qq.com>
2014-10-29 09:18:46 +0800

    change name

--- example/test-IP2CITY.class.php
+++ example/test-IP2CITY.class.php
@@ -9,5 +9,5 @@
 include dirname(__FILE__) . '/../IP2CITY.class.php';
 $ip2city = new IP2CITY("UTFWry.dat");
 $ip = $argv[1] ? $argv[1] : "58.83.177.166";
-$address = $ip2city->getAddr($ip);
+$address = $ip2city->ip2city($ip);
 echo $address . PHP_EOL;
